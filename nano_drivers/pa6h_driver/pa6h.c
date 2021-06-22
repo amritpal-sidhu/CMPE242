68c3fccd7efd653ec9ca13e1077a5c54918b14dd
amritpal-sidhu <amritpalsidhu.p@gmail.com>
2021-06-22 01:24:05 -0700

    Updated PA6H driver

--- nano_drivers/pa6h_driver/pa6h.c
+++ nano_drivers/pa6h_driver/pa6h.c
@@ -18,12 +18,8 @@ int PA6H_jetson_nano_init(void) {
     struct termios tty;
     int retval;
 
-    if ((uart_fd = open(JETSON_NANO_LINUX_UART, O_RDWR)) < 0) {
-        printf("Error %i from open: %s\n", errno, strerror(errno));
-        retval = -1;
-    }
-
     if (tcgetattr(uart_fd, &tty) != 0) {
+        printf("tcgetattr failed\n");
         retval = -1;
     }
     else {
@@ -32,13 +28,35 @@ int PA6H_jetson_nano_init(void) {
         tty.c_cflag &= ~CSIZE;
         tty.c_cflag |= CS8;
         tty.c_cflag &= ~CRTSCTS;
+        tty.c_cflag |= CREAD | CLOCAL;
+
+        tty.c_lflag &= ~ICANON;
+        tty.c_lflag &= ~ECHO;
+        tty.c_lflag &= ~ECHOE;
+        tty.c_lflag &= ~ECHONL;
+        tty.c_lflag &= ~ISIG;
+
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+        tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+        
+        tty.c_oflag &= ~OPOST;
+        tty.c_oflag &= ~ONLCR;
+
+        tty.c_cc[VMIN] = 0;
+        tty.c_cc[VTIME] = 0;
 
         cfsetispeed(&tty, B9600);
         cfsetospeed(&tty, B9600);
 
         if (tcsetattr(uart_fd, TCSANOW, &tty) != 0) {
+            printf("tcsetattr failed\n");
             retval = -1;
         }
+    }
+
+    if (retval != -1 && (uart_fd = open(JETSON_NANO_LINUX_UART, O_RDWR)) < 0) {
+        printf("Error %i from open: %s\n", errno, strerror(errno));
+        retval = -1;
     }
 
     return retval;
